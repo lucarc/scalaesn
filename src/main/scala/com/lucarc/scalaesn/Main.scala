9d8be7dbbb5a84951c7bce5d0e098aaf31dec085
lucarc <luca.carcano@gmail.com>
2018-05-22 11:03:37 +0200

    included vegas viz to provide data graphing capabilities

--- src/main/scala/com/lucarc/scalaesn/Main.scala
+++ src/main/scala/com/lucarc/scalaesn/Main.scala
@@ -13,19 +13,20 @@ object Main {
   val t: Int = 10
 
   val trainX: Seq[DenseVector[Double]] = (0 until numTrain).toList.map(deg => DenseVector(breeze.numerics.sin.sinDoubleImpl(deg)))
-  val trainY: Seq[DenseVector[Double]] = (t until numTrain+t).toList.map(deg => DenseVector(breeze.numerics.sin.sinDoubleImpl(deg)))
+  val trainY: Seq[DenseVector[Double]] = (t until numTrain + t).toList.map(deg => DenseVector(breeze.numerics.sin.sinDoubleImpl(deg)))
 
   val testX: Seq[DenseVector[Double]] = (0 until numTest).toList.map(deg => DenseVector(breeze.numerics.sin.sinDoubleImpl(deg)))
-  val testY: Seq[DenseVector[Double]] = (t until numTest+t).toList.map(deg => DenseVector(breeze.numerics.sin.sinDoubleImpl(deg)))
+  val testY: Seq[DenseVector[Double]] = (t until numTest + t).toList.map(deg => DenseVector(breeze.numerics.sin.sinDoubleImpl(deg)))
 
-  val spectralRadius: Double = 0.65
-  val sparsity: Double = 0.65
+  val spectralRadius: Double = 0.7
+  val sparsity: Double = 0.80
 
 
   def main(args: Array[String]): Unit = {
 
     val esn: EchoStateNetwork = new EchoStateNetworkImplementation(reservoir = new ReservoirImplementation(nInput = 1, nNeurons = 100, sr = spectralRadius, sp = sparsity, activation = new Sigmoid), readout = new LinearRegression(reg = 1, c = 1))
     esn.fit(trainX, trainY)
+
     val predictedTestY: Seq[DenseVector[Double]] = testX.map(xi => esn.transform(x = xi))
     val gionas = (testY zip predictedTestY).flatMap(yVEP => (yVEP._1.data zip yVEP._2.data).map(y => (y._1 - y._2) * (y._1 - y._2))).sum / testY.length
     println(s"MSE $gionas")
